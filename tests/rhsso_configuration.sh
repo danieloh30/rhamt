cd54b73261bcf442e779e58212b421a252c524f6
Daniel Oh <doh@danieloh.redhat.com>
2019-03-21 14:11:22 +0900

    Updated template, rh-sso config

--- tests/rhsso_configuration.sh
+++ tests/rhsso_configuration.sh
@@ -8,6 +8,14 @@ export TKN=$(curl -k -X POST 'https://secure-rhamt-web-console-labs-infra.apps.s
  -d 'grant_type=password' \
  -d 'client_id=admin-cli' | jq -r '.access_token')
 
+ # Update a master realm with RH-SSO theme
+curl -k -X PUT 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/master/' \
+ -H "Content-Type: application/json" \
+ -H "Accept: application/json" \
+ -H "Authorization: Bearer $TKN" \
+ -d '{ "displayName": "rh-sso", "displayNameHtml": "<strong>Red Hat</strong><sup>®</sup> Single Sign On", "loginTheme": "rh-sso", "adminTheme": "rh-sso", "accountTheme": "rh-sso", "emailTheme": "rh-sso", "accessTokenLifespan": 100 }'  | jq .
+
+
 # Create a new user
 curl -k -X POST 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/rhamt/users' \
  -H "Content-Type: application/json" \
@@ -22,13 +30,18 @@ curl -k -X PUT 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.open
  -H "Authorization: Bearer $TKN" \
  -d '{ "type": "password", "value": "openshift", "temporary": true}'  | jq .
 
-
-# Get user lists
-curl -k -X GET 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/rhamt/users' \
--H "Accept: application/json" \
--H "Authorization: Bearer $TKN" | jq .
-
 # Get user ID lists
 curl -k -X GET 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/rhamt/users/' \
 -H "Accept: application/json" \
 -H "Authorization: Bearer $TKN" | jq -r '.[].id'
+
+ # Update a master realm with RH-SSO theme
+curl -k -X PUT 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/master/' \
+ -H "Content-Type: application/json" \
+ -H "Accept: application/json" \
+ -H "Authorization: Bearer $TKN" \
+ -d '{ "displayName": "rh-sso", "displayNameHtml": "<strong>Red Hat</strong><sup>®</sup> Single Sign On", "loginTheme": "rh-sso", "adminTheme": "rh-sso", "accountTheme": "rh-sso", "emailTheme": "rh-sso" }'  | jq .
+
+curl -k -X GET 'https://secure-rhamt-web-console-labs-infra.apps.seoul-250a.openshiftworkshop.com/auth/admin/realms/master' \
+-H "Accept: application/json" \
+-H "Authorization: Bearer $TKN" | jq .
